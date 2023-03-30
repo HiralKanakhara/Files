d745f81692b75225e1500454c8e4ed1722561073
HiralKanakhara <129411811+HiralKanakhara@users.noreply.github.com>
2023-03-30 20:45:23 +0530

    Add files via upload

---
+++ Arithmetic_operators.java
@@ -0,0 +1,20 @@
+public class Arithmetic_operators
+{
+	public static void main(String[] args)
+	{
+		int add = 2+3;
+		System.out.println("Addition = " + add);
+
+		int sub = 9-7;
+		System.out.println("Subtraction = " + sub);
+
+		int mul = 8*9;
+		System.out.println("Multiplication = " + mul);
+
+		int div = 9/3;
+		System.out.println("Division = " + div);
+
+		int rem = 9%2;
+		System.out.println("Remainder = " + rem);
+	}
+}
---
+++ Array.java
@@ -0,0 +1,19 @@
+public class Array
+{
+	public static void main(String[] args)
+	{
+		//declaring array
+		int anArray[];
+
+		//assigning memory
+		anArray = new int[3];
+
+		anArray[0] = 13;
+		anArray[1] = 26;
+		anArray[2] = 39;
+
+		System.out.println("Number 1 : " + anArray[0]);
+		System.out.println("Number 2 : " + anArray[1]);
+		System.out.println("Number 3 : " + anArray[2]);
+	}
+}
